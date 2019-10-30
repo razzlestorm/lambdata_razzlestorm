c901835e1fb26532801c7134954efed3ace2389a
Jerimiah Willhite <j.willhite@hotmail.com>
2019-10-29 20:47:51 -0700

    creates virtual env; adds helper functions

---
+++ lambdata_razzlestorm/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.0.1"
+my_str = "this is my_str from __init__.py"
+
---
+++ lambdata_razzlestorm/df_utils.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+ONES = pd.Series(np.ones(20))
+ZEROS = pd.Series(np.zeros(20))
+
